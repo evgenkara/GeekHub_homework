719529663fe23b6b5a26bac0bcd034c4ff8adbd8
evgenkara <karaschuk.ev@gmail.com>
2022-10-13 12:36:16 +0300

    changed task1.py

--- HT3/task1.py
+++ HT3/task1.py
@@ -4,9 +4,11 @@
 # The "replacement" value is entered by user.
 # The number of elements in the tuples must be different.
 
-list_of_tuples = [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
+list_of_tuples = [(1, 2, 3), (4, 5, 6), (7, 8, 9), ()]
 user_input = int(input('Enter value: '))
 for i in range(len(list_of_tuples)):
+    if not list_of_tuples[i]:
+        continue
     t = list(list_of_tuples[i])
     t[-1] = user_input
     list_of_tuples[i] = tuple(t)

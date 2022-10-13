5c9781ba05850b4c6de3b77eae4868f9cbd27f7a
evgenkara <karaschuk.ev@gmail.com>
2022-10-13 19:26:00 +0300

    another way solution

--- HT3/task1.py
+++ HT3/task1.py
@@ -4,14 +4,16 @@
 # The "replacement" value is entered by user.
 # The number of elements in the tuples must be different.
 
-list_of_tuples = [(1, 2, 3), (4, 5, 6), (7, 8, 9), ()]
-user_input = int(input('Enter value: '))
-for i in range(len(list_of_tuples)):
-    if not list_of_tuples[i]:
+list_of_tuples = [(1, 2, 3), (4, 5, 6, 7), (8, 9), ()]
+user_input = int(input('Enter a numer: '))
+count = 0
+for i in list_of_tuples:
+    if not i:
         continue
-    t = list(list_of_tuples[i])
+    t = list(i)
     t[-1] = user_input
-    list_of_tuples[i] = tuple(t)
+    list_of_tuples[count] = tuple(t)
+    count += 1
 
 print(list_of_tuples)
 

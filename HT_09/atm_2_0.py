7ed0d51fdfccbaa9ff5ae61b6f71d0a6bf068c61
evgenkara <karaschuk.ev@gmail.com>
2022-11-08 15:17:28 +0200

    Hometask_09 v 2.0

--- HT_09/atm_2_0.py
+++ HT_09/atm_2_0.py
@@ -1,46 +1,61 @@
-# Програма-банкомат.
-#    Використовуючи функції створити програму з наступним функціоналом:
-#       - підтримка 3-4 користувачів, які валідуються парою ім'я/пароль (файл <users.CSV>);
-#       - кожен з користувачів має свій поточний баланс (файл <{username}_balance.TXT>) та історію транзакцій
-#       (файл <{username_transactions.JSON>);
-#       - є можливість як вносити гроші, так і знімати їх. Обов'язкова перевірка введених даних
-#       (введено цифри; знімається не більше, ніж є на рахунку і т.д.).
-#    Особливості реалізації:
-#       - файл з балансом - оновлюється кожен раз при зміні балансу (містить просто цифру з балансом);
-#       - файл - транзакціями - кожна транзакція у вигляді JSON рядка додається в кінець файла;
-#       - файл з користувачами: тільки читається. Але якщо захочете реалізувати функціонал додавання
-#       нового користувача - не стримуйте себе :)
-#    Особливості функціонала:
-#       - за кожен функціонал відповідає окрема функція;
-#       - основна функція - <start()> - буде в собі містити весь workflow банкомата:
-#       - на початку роботи - логін користувача (програма запитує ім'я/пароль).
-#       Якщо вони неправильні - вивести повідомлення про це і закінчити роботу
-#       (хочете - зробіть 3 спроби, а потім вже закінчити роботу - все на ентузіазмі :))
-#       - потім - елементарне меню типн:
-#         Введіть дію:
-#            1. Подивитись баланс
-#            2. Поповнити баланс
-#            3. Вихід
+# Банкомат 2.0
+#     - усі дані зберігаються тільки в sqlite3 базі даних у відповідних таблицях. Більше ніяких файлів. Якщо в попередньому завданні ви добре продумали структуру програми то у вас не виникне проблем швидко адаптувати її до нових вимог.
+#     - на старті додати можливість залогінитися або створити нового користувача (при створенні нового користувача, перевіряється відповідність логіну і паролю мінімальним вимогам. Для перевірки створіть окремі функції)
+#     - в таблиці з користувачами також має бути створений унікальний користувач-інкасатор, який матиме розширені можливості (домовимось, що логін/пароль будуть admin/admin щоб нам було простіше перевіряти)
+#     - банкомат має власний баланс
+#     - кількість купюр в банкоматі обмежена (тобто має зберігатися номінал та кількість). Номінали купюр - 10, 20, 50, 100, 200, 500, 1000
+#     - змінювати вручну кількість купюр або подивитися їх залишок в банкоматі може лише інкасатор
+#     - користувач через банкомат може покласти на рахунок лише суму кратну мінімальному номіналу що підтримує банкомат. В іншому випадку - повернути "здачу" (наприклад при поклажі 1005 --> повернути 5). Але це не має впливати на баланс/кількість купюр банкомату, лише збільшується баланс користувача (моделюємо наявність двох незалежних касет в банкоматі - одна на прийом, інша на видачу)
+#     - зняти можна лише в межах власного балансу, але не більше ніж є всього в банкоматі.
+#     - при неможливості виконання якоїсь операції - вивести повідомлення з причиною (невірний логін/пароль, недостатньо коштів на рахунку, неможливо видати суму наявними купюрами тощо.)
+#     - файл бази даних з усіма створеними таблицями і даними також додайте в репозиторій, що б ми могли його використати
 
 
 
 import sqlite3
-import csv
-import json
 import datetime
 
 
 
+class BankException(Exception):
+    pass
+
+
+con = sqlite3.connect('atm.db')
+cur = con.cursor()
+
+
+def create_tables():
+    cur.execute('''CREATE TABLE IF NOT EXISTS users
+                    (id integer PRIMARY KEY, username text, password text,
+                    is_incasator BOOLEAN NOT NULL CHECK (is_incasator IN (0,1)), UNIQUE (username)) ''')
+    cur.execute('''CREATE TABLE IF NOT EXISTS balance
+                    ( id integer PRIMARY KEY,
+                    username text,
+                    user_balance integer,
+                    FOREIGN KEY (id) REFERENCES users(id))''')
+    cur.execute('''CREATE TABLE IF NOT EXISTS users_transactions (id integer, username text, operation text, date_time timestamp, operation_summa integer, balance_after_oper integer, FOREIGN KEY (id) REFERENCES users(id))''')
+    cur.execute('''CREATE TABLE IF NOT EXISTS banknotes (key integer PRIMARY KEY, banknote integer, amount integer )''')
+    con.commit()
+
+
+def insert_tables():
+    cur.execute('''INSERT OR IGNORE INTO users VALUES (1, 'user1', '12345', 0), (2, 'user2', '23456', 0), (3, 'user3', '34567', 0), (4, 'admin', 'admin', 1)''')
+    cur.execute('''INSERT OR IGNORE INTO balance VALUES (1, 'user1', 50000), (2, 'user2', 45000), (3, 'user3', 55000), (4, 'admin', 75000)''')
+    cur.execute('''INSERT OR IGNORE INTO banknotes VALUES (1, 1000, 50), (2, 500, 50), (3, 200, 100), (4, 100, 100), (5, 50, 200), (6, 20, 200), (7, 10, 200)''')
+    con.commit()
+
+
 def validate_user(username, password):
-    user_info = [username, password]
     login_check = False
-    with open('users.csv', 'r') as users_data:
-        data = csv.reader(users_data)
-        for user in data:
-            if user_info == user:
-                login_check = True
-                break
-    return login_check
+    is_admin = False
+    cur.execute('SELECT is_incasator FROM users WHERE username = ? AND password = ?', (username, password))
+    user = cur.fetchone()
+    if user:
+        login_check = True
+        if 1 in user:
+            is_admin = True
+    return login_check, is_admin
 
 
 def add_transaction(username, summa, new_sum):
@@ -48,86 +63,168 @@ def add_transaction(username, summa, new_sum):
         transaction = 'Пополнение счета'
     else:
         transaction = 'Получение наличных'
+    cur.execute('SELECT id FROM users WHERE username = ?', (username,))
+    user_id = cur.fetchone()[0]
     date_time = datetime.datetime.now().strftime('%d.%m.%Y, %H:%M')
-    data = {'Дата, время': date_time, 'Операция': transaction, 'Сумма операции': summa, 'Баланс после операции': new_sum}
-    with open(f'{username}_transactions.json', 'a', encoding='utf-8') as transactions:
-        json.dump(data, transactions, ensure_ascii=False)
-        transactions.write('\n')
+    cur.execute(f'INSERT INTO users_transactions VALUES (?, ?, ?, ?, ?, ?)', (user_id, username, transaction, date_time, summa, new_sum))
+    con.commit()
 
 
 def check_balance(username):
-    with open(f'{username}_balance.txt', 'r') as balance:
-        currency = int(balance.readline())
-        return f'У Вас на счету: {currency}'
+    cur.execute('SELECT user_balance FROM balance WHERE username = ?', (username,))
+    balance = cur.fetchone()[0]
+    return f'Ваш баланс: {str(balance)}'
 
 
 def replenish(username):
-    with open(f'{username}_balance.txt', 'r') as balance:
-        currency = int(balance.readline())
     summa = int(input('Сумма пополнения: '))
+    cur.execute('SELECT user_balance FROM balance WHERE username = ?', (username,))
+    currency = cur.fetchone()[0]
     if summa >= 0:
         new_balance = currency + summa
-        with open(f'{username}_balance.txt', 'w') as balance:
-            balance.write(str(new_balance))
+        cur.execute('UPDATE balance SET user_balance = ? WHERE username = ?', (new_balance, username))
         add_transaction(username, summa, new_balance)
         print(f'Счет пополнен на {summa}')
     else:
         print('Не могу распознать купюры...')
 
 
+def withdraw_balance(num):
+    cur.execute('SELECT banknote, amount FROM banknotes')
+    load_banknotes = {bank[0]: bank[1] for bank in cur.fetchall()}
+    keys = list(map(int, load_banknotes.keys()))
+    wit_banknotes = []
+    counter = 0
+    while sum(wit_banknotes) < num:
+        skip = False
+        if counter + 1 > len(keys):
+            raise BankException
+        for key in keys:
+            if skip:
+                break
+            elif key + sum(wit_banknotes) <= num and load_banknotes[key] > 0:
+                for k in keys:
+                    if (num - sum(wit_banknotes) - key) % k == 0 and load_banknotes[k] > 0:
+                        wit_banknotes.append(key)
+                        load_banknotes[key] -= 1
+                        skip = True
+                        break
+        counter += 1
+    for key, val in zip(load_banknotes.keys(), load_banknotes.values()):
+        cur.execute('UPDATE banknotes SET amount = ? WHERE banknote = ?', (val, key))
+        con.commit()
+    return f'Выдано купюры: {wit_banknotes}'
+
+
 def withdraw(username):
-    with open(f'{username}_balance.txt', 'r') as balance:
-        currency = int(balance.readline())
-    summa = int(input('Укажите сумму для вывода: '))
-    if summa > 0:
+    atm_sum = 0
+    cur.execute('SELECT banknote, amount FROM banknotes')
+    for bank in cur.fetchall():
+        atm_sum += bank[0] * bank[1]
+    cur.execute('SELECT user_balance FROM balance WHERE username = ?', (username,))
+    currency = cur.fetchone()[0]
+
+    summa = int(input(f'Сумма наличных в банкомате: {atm_sum}\nУкажите сумму для вывода: '))
+    if summa > 0 and summa % 10 == 0:
         if summa <= currency:
             new_balance = currency - summa
-            with open(f'{username}_balance.txt', 'w') as balance:
-                balance.write(str(new_balance))
-            add_transaction(username, summa, new_balance)
-            print(f'Вы сняли со счета {summa}')
+            cur.execute('UPDATE balance SET user_balance = ? WHERE username = ?', (new_balance, username))
+            add_transaction(username, currency, new_balance)
+            try:
+                print(withdraw_balance(summa))
+            except BankException:
+                w = int(input(f'В банкомате недостаточно денег. Доступно: {atm_sum}'))
+                print(withdraw_balance(w))
         else:
-            print('На Вашем счету недостаточно денег :(')
-            withdraw(username)
+            w = int(input('На Вашем счету недостаточно денег :(. Введите сумму меньше: '))
+            print(withdraw_balance(w))
+
     else:
-        print('Проверьте сумму')
+        print('Сумма должна быть кратна 10.')
+        withdraw(username)
+    con.commit()
 
 
-def back():
-    choose = int(input('1. На главный экран\n2. Завершить работу\n> '))
-    if choose == 1:
-        start()
-    elif choose == 2:
-        exit()
+def register():
+    new_user = input('Имя пользователя: ')
+    new_password = input('Пароль: ')
+    not_occupied = True
+    cur.execute('SELECT * FROM users WHERE username = ?', (new_user,))
+    user = cur.fetchall()
+    cur.execute('SELECT max(id) FROM users')
+    last_id = cur.fetchone()[0]
+    if user:
+        not_occupied = False
+    if not_occupied:
+        cur.execute('INSERT INTO users VALUES (?, ?, ?, ?)', (last_id + 1, new_user, new_password, 0))
+        cur.execute('INSERT INTO balance VALUES (?, ?, ?)', (last_id + 1, new_user, 0))
+        con.commit()
+    else:
+        print('Имя пользователя занято.')
 
 
-def start():
-    if validate_user(login, password):
-        action = int(input('Выберите операцию:\n1. Баланс на экран\n2. Пополнить счет\n3. Получить наличные\n4. Завершить работу\n> '))
-        if action == 1:
-            print(check_balance(login))
-            back()
-        elif action == 2:
-            replenish(login)
-            back()
-        elif action == 3:
-            withdraw(login)
-            back()
-        elif action == 4:
+def check_banknotes():
+    cur.execute('SELECT banknote, amount FROM banknotes')
+    result = {bank[0]: bank[1] for bank in cur.fetchall()}
+    return result
+
+
+def change_banknotes():
+    choice = input('Выберите номинал: ')
+    cur.execute('SELECT * FROM banknotes WHERE banknote = ?', (choice,))
+    if cur.fetchone():
+        new = int(input('Новое значение: '))
+        if new >= 0:
+            cur.execute('UPDATE banknotes SET amount = ? WHERE banknote = ?', (new, choice))
+        else:
+            print('Значение не может быть отрицательным\n>')
+            change_banknotes()
+    else:
+        print('Выберите из доступных значений (10, 20, 50, 100, 200, 500, 1000)')
+        change_banknotes()
+    con.commit()
+
+
+def start(username, password):
+    create_tables()
+    insert_tables()
+    if validate_user(username, password)[0]:
+        while True:
+            if validate_user(username, password)[1]:
+                while True:
+                    action = int(input('Выберите операцию:\n1. Показать доступные купюры\n2. Изменить наличие купюр\n3. Завершить работу\n> '))
+                    if action == 1:
+                        print(check_banknotes())
+                    elif action == 2:
+                        change_banknotes()
+                    elif action == 3:
+                        exit()
+                    else:
+                        print('Что-то пошло не так. Попробуйте ещё раз.\n>')
+            action = int(input('Выберите операцию:\n1. Баланс на экран\n2. Пополнить счет\n3. Получить наличные\n4. Завершить работу\n> '))
+            if action == 1:
+                print(check_balance(username))
+            elif action == 2:
+                replenish(username)
+            elif action == 3:
+                withdraw(username)
+            elif action == 4:
+                exit()
+            else:
+                print('Что-то пошло не так. Попробуйте ещё раз.\n>')
+    else:
+        reg = int(input('Вы не зарегистрированы. Пройти регистрацию\n1. Да\n2. Нет\n> '))
+        if reg == 1:
+            register()
+        else:
             exit()
-        else:
-            print('Что-то пошло не так. Попробуйте ещё раз.\n')
-            start()
-    else:
-        print('Неверный логин или пароль.')
 
 
 
 if __name__ == '__main__':
-    login = input('Введите имя: ')
-    password = input('Enter password: ')
-    try:
-        start()
-    except ValueError:
-        print('Что-то пошло не так. Попробуйте ещё раз.')
-        start()
+    username = input('Введите имя: ')
+    password = input('Введите пароль: ')
+
+    start(username, password)
+
+    con.close()
